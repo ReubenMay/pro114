b8d65bf6a1fd465dc50ef0c3f840c66027130016
ReubenMay <95076512+ReubenMay@users.noreply.github.com>
2023-03-17 18:28:53 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -11,3 +11,12 @@ function draw(){
 function take_snapshot(){
     save("reubenrex.png");
 }
+function gotPoses(results){
+    if(results.length>0)
+    {
+        console.log(results);
+        console.log("nose x ="+results[0].pose.nose.x);
+        console.log("nose y ="+results[0].pose.nose.y);
+
+    }
+}
